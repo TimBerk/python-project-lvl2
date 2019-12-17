766899d2045b33327d9488515472570390284062
Lysak Tatiana <lysak-tatiana2501@ya.ru>
2019-12-17 12:20:54 +0300

    Added help information for format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,7 +6,8 @@ def main():
     parser = argparse.ArgumentParser(description='Generate diff')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
-    parser.add_argument('-f', type=str, dest='format')
+    parser.add_argument('-f', type=str, dest='format',
+                        help="set format of output")
     args = parser.parse_args()
     print(args.indir)
 
